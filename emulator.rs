3b03b426cf4ec773df77d35681a842e1cf1397e3
Wilfred Hughes <me@wilfred.me.uk>
2015-05-05 23:05:29 +0100

    Fix indentation.

--- emulator.rs
+++ emulator.rs
@@ -5,24 +5,24 @@ type StackPointer = u16;
 
 #[derive(Debug)]
 struct CPU {
-        // Generic registers.
-        a: Register,
-        b: Register,
-        c: Register,
-        d: Register,
-        e: Register,
-        h: Register,
-        l: Register,
+    // Generic registers.
+    a: Register,
+    b: Register,
+    c: Register,
+    d: Register,
+    e: Register,
+    h: Register,
+    l: Register,
 
-        flags: Register,
+    flags: Register,
 
-        // Program state.
-        pc: ProgramCounter,
-        sp: StackPointer,
+    // Program state.
+    pc: ProgramCounter,
+    sp: StackPointer,
 
-        // Clock.
-        m: Register,
-        t: Register,
+    // Clock.
+    m: Register,
+    t: Register,
 }
 
 static ZERO_FLAG: u8 = 0x80;
@@ -32,32 +32,32 @@ static CARRY_FLAG: u8 = 0x10;
 
 
 fn addr_e (cpu: &mut CPU) {
-        //! Add E to A, leaving result in A (ADD A, E)
+    //! Add E to A, leaving result in A (ADD A, E)
 
-        // We use a larger temporary, so we can detect overflow.
-        let mut result: u16 = cpu.a as u16 + cpu.e as u16;
+    // We use a larger temporary, so we can detect overflow.
+    let mut result: u16 = cpu.a as u16 + cpu.e as u16;
 
-        cpu.flags = 0;
-        if result == 0 {
-                cpu.flags |= ZERO_FLAG;
-        }
-        if result > 255 {
-                cpu.flags |= CARRY_FLAG;
-        }
-        
-        result = result & 255;
-        cpu.a = result as Register;
+    cpu.flags = 0;
+    if result == 0 {
+        cpu.flags |= ZERO_FLAG;
+    }
+    if result > 255 {
+        cpu.flags |= CARRY_FLAG;
+    }
+    
+    result = result & 255;
+    cpu.a = result as Register;
 
-        cpu.m = 1; cpu.t = 4;
+    cpu.m = 1; cpu.t = 4;
 }
 
 fn main() {
-        let mut cpu = CPU {
-                a: 254, b: 0, c: 0, d: 0, e: 2, h: 0,
-                l: 0, flags: 0, pc: 0, sp: 0, m: 0, t: 0,
-        };
-        println!("Initial CPU state: {:?}", cpu);
+    let mut cpu = CPU {
+        a: 254, b: 0, c: 0, d: 0, e: 2, h: 0,
+        l: 0, flags: 0, pc: 0, sp: 0, m: 0, t: 0,
+    };
+    println!("Initial CPU state: {:?}", cpu);
 
-        addr_e(&mut cpu);
-        println!("Final CPU state:   {:?}", cpu);
+    addr_e(&mut cpu);
+    println!("Final CPU state:   {:?}", cpu);
 }
