73491a7ade191d71fd6536ca60973e5cfd5c8563
Wilfred Hughes <me@wilfred.me.uk>
2015-10-28 23:18:41 +0000

    Print the raw bytes with offsets.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,14 @@ fn read_bytes(path: &str) -> Result<Vec<u8>, std::io::Error> {
     Ok(bytes)
 }
 
+fn print_instrs(bytes: &[u8]) {
+    println!("OFFSET BYTE");
+
+    for (offset, byte) in bytes.iter().enumerate() {
+        println!("{:X}      0x{:X}", offset, byte);
+    }
+}
+
 #[cfg_attr(test, allow(dead_code))]
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -21,7 +29,7 @@ fn main() {
         let path = &args[1];
         match read_bytes(path) {
             Ok(bytes) => {
-                println!("Read {:?} bytes", bytes.len());
+                print_instrs(&bytes[..]);
             }
             Err(_) => {
                 println!("Could not read file: {}", path);
